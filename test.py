f18dd87cb5ed7d4b0d2ceb4510db8847919b8046
basbhai <basbhai@live.com>
2026-05-09 18:59:23 +0545

    commit test

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from AI import ask_deepseek_text
 
 
-print(ask_deepseek_text("What is property of car give me in json, just pure json without any text, just json"))
+print(ask_deepseek_text("What is ok property of car give me in json, just pure json without any text, just json"))
